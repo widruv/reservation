906f18eb5b3fe9b22a80cb412c68ed9eff9ead1f
hjkwon <hjkwon@pentasecurity.com>
2018-12-17 12:49:44 +0900

    insert show function

---
+++ cal.js
@@ -0,0 +1,27 @@
+var FullCalendar = {
+    lists : [],
+    insert:function(title, start, end, color='blue') {
+        this.lists.push({
+            title: title,
+            start: start,
+            end: end,
+            color: color
+        });
+    },
+    show:function(id) {
+        $(id).fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      defaultDate: '2018-03-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: this.lists
+
+    });
+
+    } 
+}
